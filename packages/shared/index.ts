354466da43e36d0058e58e3f67968be5bae80703
Husni Adil Makmur <husni.adil@gmail.com>
2024-07-20 00:54:21 +0700

    Export from index

--- packages/shared/index.ts
+++ packages/shared/index.ts
@@ -1,4 +1,14 @@
+import { type ChatHistory, UpstashRedisChatHistory } from "./chat-history";
+import { UpstashRedisLock } from "./lock";
+import { UpstashRedisRateLimit } from "./rate-limit";
 import { sendMessage } from "./telegram";
 import { getCurrentDateTime } from "./time";
 
-export { getCurrentDateTime, sendMessage as sendTelegramMessage };
+export {
+  getCurrentDateTime,
+  sendMessage as sendTelegramMessage,
+  type ChatHistory,
+  UpstashRedisChatHistory,
+  UpstashRedisLock,
+  UpstashRedisRateLimit,
+};

0bc102fe248177f79ba1b0eea53362067b434810
Husni Adil Makmur <husni.adil@gmail.com>
2024-07-20 00:20:00 +0700

    Remove langchain deps, use openai instead

--- packages/aisyah-ai-whisper/src/whisper.ts
+++ packages/aisyah-ai-whisper/src/whisper.ts
@@ -1,22 +1,20 @@
-import { OpenAIClient } from "@langchain/openai";
+import { OpenAI } from "openai";
 
 export class Whisper {
-  private readonly openAIClient: OpenAIClient;
+  private readonly openAI: OpenAI;
 
   constructor(env: { OPENAI_API_KEY: string }) {
-    this.openAIClient = new OpenAIClient({
+    this.openAI = new OpenAI({
       apiKey: env.OPENAI_API_KEY,
     });
   }
 
   async listen(audioUrl: string) {
     try {
-      const transcription = await this.openAIClient.audio.transcriptions.create(
-        {
-          file: await fetch(audioUrl),
-          model: "whisper-1",
-        },
-      );
+      const transcription = await this.openAI.audio.transcriptions.create({
+        file: await fetch(audioUrl),
+        model: "whisper-1",
+      });
       return transcription.text;
     } catch (error) {
       console.error("Failed to transcribe audio:", error);

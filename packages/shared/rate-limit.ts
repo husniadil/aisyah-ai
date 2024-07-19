f3dcc89f93d43b3c1ee96c4843e98af64d25ac4f
Husni Adil Makmur <husni.adil@gmail.com>
2024-07-20 00:52:25 +0700

    Add rate-limit util

---
+++ packages/shared/rate-limit.ts
@@ -0,0 +1,75 @@
+import { Redis } from "@upstash/redis/cloudflare";
+
+interface Env {
+  UPSTASH_REDIS_REST_URL: string;
+  UPSTASH_REDIS_REST_TOKEN: string;
+}
+
+export class UpstashRedisRateLimit {
+  private readonly redis: Redis;
+  private readonly keyPrefix = "rate_limit";
+  private readonly limit: number;
+
+  constructor(env: Env, limit = 100) {
+    if (limit <= 0) {
+      throw new Error("Limit must be a positive integer");
+    }
+    this.redis = new Redis({
+      url: env.UPSTASH_REDIS_REST_URL,
+      token: env.UPSTASH_REDIS_REST_TOKEN,
+    });
+    this.limit = limit;
+  }
+
+  public async isRateLimited(key: string): Promise<boolean> {
+    const fullKey = this.getFullKey(key);
+    try {
+      const value = await this.redis.get<number>(fullKey);
+      if (value) {
+        return await this.handleExistingKey(fullKey, value);
+      }
+    } catch (error) {
+      console.warn(`Error checking rate limit for ${key}:`, error);
+      return false;
+    }
+    await this.initializeKey(fullKey);
+    return false;
+  }
+
+  private getFullKey(key: string): string {
+    return `${this.keyPrefix}:${key}`;
+  }
+
+  private async handleExistingKey(
+    fullKey: string,
+    count: number,
+  ): Promise<boolean> {
+    if (count >= this.limit) {
+      return true;
+    }
+    await this.redis.incr(fullKey);
+    return false;
+  }
+
+  private async initializeKey(fullKey: string): Promise<void> {
+    const now = new Date();
+    const endOfDay = new Date(
+      Date.UTC(
+        now.getUTCFullYear(),
+        now.getUTCMonth(),
+        now.getUTCDate(),
+        23,
+        59,
+        59,
+      ),
+    );
+    const secondsUntilEndOfDay = Math.floor(
+      (endOfDay.getTime() - now.getTime()) / 1000,
+    );
+    await this.redis
+      .multi()
+      .incr(fullKey)
+      .expire(fullKey, secondsUntilEndOfDay)
+      .exec();
+  }
+}

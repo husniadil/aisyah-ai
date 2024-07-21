5cef1586f1c28ce01c6457ea5b08c52b75cba0fd
Husni Adil Makmur <husni.adil@gmail.com>
2024-07-21 19:27:13 +0700

    Fix timezone

--- packages/shared/time.ts
+++ packages/shared/time.ts
@@ -1,6 +1,43 @@
 import type { outputSchema } from "@packages/shared/types/time";
 import type { z } from "zod";
 
-export const getCurrentDateTime = (): z.infer<typeof outputSchema> => {
-  return new Date().toISOString();
+export const getCurrentDateTime = (
+  timezone: string,
+): z.infer<typeof outputSchema> => {
+  const date = new Date();
+
+  const pad = (num: number) => num.toString().padStart(2, "0");
+
+  const formatter = new Intl.DateTimeFormat("en-GB", {
+    timeZone: timezone,
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+  });
+
+  const [
+    { value: day },
+    ,
+    { value: month },
+    ,
+    { value: year },
+    ,
+    { value: hour },
+    ,
+    { value: minute },
+    ,
+    { value: second },
+  ] = formatter.formatToParts(date);
+
+  const offset = date.getTimezoneOffset();
+  const offsetHours = Math.floor(Math.abs(offset) / 60);
+  const offsetMinutes = Math.abs(offset) % 60;
+  const sign = offset > 0 ? "-" : "+";
+  const formattedOffset = `${sign}${pad(offsetHours)}${pad(offsetMinutes)}`;
+
+  return `${year}-${month}-${day} ${hour}:${minute}:${second} ${formattedOffset}`;
 };

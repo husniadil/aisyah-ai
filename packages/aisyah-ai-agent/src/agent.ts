a216c5631ab042830cec93aafbbfc7050eedcffd
Husni Adil Makmur <husni.adil@gmail.com>
2024-07-21 19:47:35 +0700

    Fix timezone

--- packages/aisyah-ai-agent/src/agent.ts
+++ packages/aisyah-ai-agent/src/agent.ts
@@ -119,8 +119,9 @@ export class Agent implements IAgent {
   }
 
   private formatOutput(output: string): string {
-    const regex = /^\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\] \w+:\s*|\w+:\s*/;
-    return output.replace(regex, "").trim();
+    return output
+      .replace(/\[\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z\] \w+: /, "")
+      .replace(/^\w+: /, "");
   }
 
   async chat(
@@ -137,7 +138,7 @@ export class Agent implements IAgent {
       .invoke({
         system_message: new SystemMessage(this.systemPrompt),
         current_time: new SystemMessage(
-          `Context:\ncurrent datetime: ${getCurrentDateTime()}\ncurrent timezone: Asia/Jakarta`,
+          `Context:\ncurrent date-time in UTC: ${getCurrentDateTime()}\ncurrent timezone: Asia/Jakarta`,
         ),
         chat_id: new SystemMessage(`Context: chatId: ${chatId}`),
         message_id: new SystemMessage(`Context: messageId: ${messageId}`),
